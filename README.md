b8f84a0c02f791c8ac5e8343188f023fffcbcbc3
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2021-01-06 13:06:43 +0700

    base tag href and cache

--- README.md
+++ README.md
@@ -16,5 +16,8 @@ then open the server at http://localhost:3000
 
 example:
 http://localhost:3000/?target_url=https://google.com
-or
-http://localhost:3000/?target_url=https://google.com&force_base_tag_url=https://mydomain.com
+
+query params:
+- `target_url`: any url to be previewed as cached page
+- `base_tag_href`: any url, will replace html base tag's href
+- `force_fresh`: `true` or `false`, if true every request will refetch the original page
--- index.js
+++ index.js
@@ -3,28 +3,40 @@ const app = express()
 const port = 3000
 const puppeteer = require('puppeteer');
 
+const cache = {}
+
 app.get('/', async (req, res) => {
   const targetUrl = req.query.target_url
-  const forceBaseTagUrl = req.query.force_base_tag_url
+  const baseTagHref = req.query.base_tag_href
+  const forceFresh = req.query.forceFresh === 'true'
+  const cacheKey = `${targetUrl}-${baseTagHref}`
+  const cachedData = cache[cacheKey]
+  const useCachedData = Boolean(!forceFresh && cachedData)
+
   let status
 
   try {
+    if (useCachedData) {
+      return res.send(cachedData)
+    }
+
     const browser = await puppeteer.launch()
     const [page] = await browser.pages()
     await page.goto(targetUrl, { waitUntil: 'networkidle0' })
     let data = await page.evaluate(() => document.querySelector('*').outerHTML)
-    if (forceBaseTagUrl) {
-      data = data.replace(/<base href=".*">/g, `<base href="${forceBaseTagUrl}">`)
+    if (baseTagHref) {
+      data = data.replace(/<base href=".*">/g, `<base href="${baseTagHref}">`)
     }
     res.send(data)
     status = 'success'
+    cache[cacheKey] = data
     await browser.close()
   } catch (err) {
     console.error(err)
     status = 'failed'
     res.send(err.message)
   } finally {
-    console.log({targetUrl, status})
+    console.log({targetUrl, status, useCachedData})
   }
 })
 
